224c7e64febd4727d42323a686226cdb85eb1fd5
David Serrano <99d.serrano@gmail.com>
2022-03-09 18:41:51 +0100

    Things to solve in model.py \_O_/

--- week1/model.py
+++ week1/model.py
@@ -6,23 +6,37 @@ from collections import OrderedDict
 class NetSquared(nn.Module):
     def __init__(self):
         super(NetSquared, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3,3)),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-            nn.BatchNorm2d(num_features=16),
-            nn.Dropout(p=0.1),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3)),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),
-            nn.BatchNorm2d(num_features=32),
-            nn.Dropout(p=0.3),
-            nn.Linear()
-        )
+
+        self.relu = nn.ReLU()
+        self.maxp = nn.MaxPool2d(kernel_size=2)
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3,3))
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3))
+
+        self.bnorm1 = nn.BatchNorm2d(num_features=32)
+        self.bnorm2 = nn.BatchNorm2d(num_features=64)
+
+        self.dropout1 = nn.Dropout2d(p=0.1)
+        self.dropout2 = nn.Dropout2d(p=0.3)
+
+        self.linear = nn.Linear(in_features=186624, out_features=8)
 
     def forward(self, x):
-        return self.model(x)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxp(x)
+        x = self.bnorm1(x)
 
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxp(x)
+        x = self.bnorm2(x)
+
+        x = torch.squeeze(x)
+        x = torch.flatten(x,1)
+        x = self.linear(x)
+
+        return x
 
 if __name__ == "__main__":
     """
