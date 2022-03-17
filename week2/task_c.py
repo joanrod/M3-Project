1516df69264d3a4dad5e5d0bf8ed17f8910811e6
Juan <juanantonio.rodriguez@upf.edu>
2022-03-17 21:19:22 +0100

    taskc

--- week2/task_c.py
+++ week2/task_c.py
@@ -17,15 +17,18 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
-detectron_model = "segmentation" # detection or segmentation
+detectron_type = "segmentation" # detection or segmentation
 
 cfg = get_cfg()
 # add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
-if detectron_model == "detection":
+if detectron_type == "detection":
 	cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"))
 	cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-	# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
 	cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml")
+else:
+	cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"))
+	cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+	cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml")
 
 # Load image from KITTI-MOTS
 path_data = '/home/mcv/datasets/KITTI-MOTS/testing/image_02/'
@@ -45,5 +48,4 @@ print(outputs["instances"].pred_boxes)
 v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
 out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
-cv2.imwrite('task_c_train_0000_'+image_name+'_detections.png', out.get_image()[:, :, ::-1])
-cv2.imwrite('task_c_train_0000_'+image_name+'_orig.png', im)
+cv2.imwrite('task_c_train_'+sequence+'_'+image_name+'_'+detectron_type+'.png', out.get_image()[:, :, ::-1])
