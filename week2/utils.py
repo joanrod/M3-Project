0d491cc9b239e60d8de5ff08c2dfbc52fd6e59c0
David Serrano <61697910+davidserra9@users.noreply.github.com>
2022-03-20 20:03:44 +0100

    Nice progress on task e

--- week2/utils.py
+++ week2/utils.py
@@ -2,11 +2,24 @@ import torch
 import copy
 import random
 import cv2
+import os
 
-from detectron2.engine import HookBase
+from detectron2.engine import HookBase, DefaultTrainer
 from detectron2.data import MetadataCatalog, build_detection_train_loader
 from detectron2.utils import comm   # For multi-gpu communication
 from detectron2.utils.visualizer import Visualizer
+from detectron2.evaluation import COCOEvaluator
+
+class MyTrainer(DefaultTrainer):
+    """
+    Custom trainer to be able to compute AP in the middle of the training
+    From: https://eidos-ai.medium.com/training-on-detectron2-with-a-validation-set-and-plot-loss-on-it-to-avoid-overfitting-6449418fbf4e
+    """
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR,"inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
 
 class ValidationLoss(HookBase):
     """
@@ -24,7 +37,7 @@ class ValidationLoss(HookBase):
     def __init__(self, cfg):
         super().__init__() # takes init from HookBase
         self.cfg = cfg.clone()
-        self.cfg.DATASETS.TRAIN = cfg.DATASETS.TEST
+        self.cfg.DATASETS.TRAIN = cfg.DATASETS.VAL
         self._loader = iter(build_detection_train_loader(self.cfg)) # builds the dataloader from the provided cfg
         self.best_loss = float('inf') # Current best loss, initially infinite
         self.weights = None # Current best weights, initially none
@@ -36,7 +49,6 @@ class ValidationLoss(HookBase):
             print(f"Hello at iteration {self.trainer.iter}!") # print the current iteration if it's divisible by 100
 
         data = next(self._loader) # load the next piece of data from the dataloader
-
         with torch.no_grad(): # disables gradient calculation; we don't need it here because we're not training, just calculating the val loss
             loss_dict = self.trainer.model(data) # more about it in the next section
 
@@ -67,3 +79,27 @@ def show_results(cfg, dataset_dicts, predictor, samples=10):
         v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
         inference_img = v.get_image()[:, :, ::-1]
         cv2.imwrite(f'test{idx}.png', inference_img)
+
+# txt = open("kitti_splits/kitti_test.txt", 'r')  # Open the text file in which there are the sequences of the corresponding split
+#     txt_lines = txt.read().splitlines()  # Read all the lines of the text file
+#
+#     line = txt_lines[0]
+#     frames = []
+#     folder_path = os.path.join(kitti_path, line)
+#     # Iterate through the images of the corresponding path
+#     for img_path in sorted(glob.glob(f'{folder_path}/*.png')):
+#         print(img_path)
+#         im = cv2.imread(os.path.join(img_path))
+#         output_dict = predictor(im)
+#         v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TEST[0]), scale=1.2)
+#
+#         for cidx, (bbox, id) in enumerate(zip(output_dict["instances"].pred_boxes.tensor, output_dict["instances"].pred_classes)):
+#             v.draw_box(bbox.cpu(), edge_color=(1,0,0), alpha=0.9)
+#
+#         frames.append(v.get_output().get_image()[:, :, ::-1])
+#
+#     print(frames[0].shape[:2])
+#     video = cv2.VideoWriter('test.avi', cv2.VideoWriter_fourcc(*'DIVX'), 10, frames[0].shape[:2])
+#     for frame in frames:
+#         video.write(frame)
+#     video.release()
